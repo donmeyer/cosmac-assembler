3265d97b2a38a4d2f09e11a8d13eee74fcde21bd
Don Meyer <islanddon@mac.com>
2018-09-22 22:12:36 -0700

    Add the regression test files.

---
+++ test/README.md
@@ -0,0 +1,9 @@
+These are test files for the cosmac assembler.
+
+They confirm that the test source build has not changed as the asembler evolves.
+
+Run the test script `test.sh`  from this directory.
+
+
+The 'reference' folder holds "known good" assembler output files. These are compared to the latest assembler output.
+These files should be updated as needed when *valid* changes to the assembler occur.
---
+++ test/test.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+
+echo Compiling
+../cosmacasm.py  --quiet test.src
+
+echo ----------------------------------------
+echo Testing test hex
+cmp --verbose test.hex reference/test.hex
+
+echo ----------------------------------------
+echo Testing test listing
+cmp test.lst reference/test.lst
+
+
+echo ----------------------------------------
+echo Compiling
+../cosmacasm.py  --quiet test_dc.src
+
+echo ----------------------------------------
+echo Testing test_dc hex
+cmp --verbose test_dc.hex reference/test_dc.hex
+
+echo ----------------------------------------
+echo Testing test_dc listing
+cmp test_dc.lst reference/test_dc.lst
+
+
+echo ----------------------------------------
+echo Compiling
+../cosmacasm.py  --quiet test_exp.src
+
+echo ----------------------------------------
+echo Testing test_exp hex
+cmp --verbose test_exp.hex reference/test_exp.hex
+
+echo ----------------------------------------
+echo Testing test_exp listing
+cmp test_exp.lst reference/test_exp.lst
+
+
+
+echo ----------------------------------------
+echo Compiling
+../cosmacasm.py --quiet FIG_Forth.src
+
+echo ----------------------------------------
+echo Testing FIG hex
+cmp FIG_Forth.hex reference/FIG_Forth.hex
+
+echo ----------------------------------------
+echo Testing FIG listing
+cmp FIG_Forth.lst reference/FIG_Forth.lst
+
+
+echo
+echo Tests completed
